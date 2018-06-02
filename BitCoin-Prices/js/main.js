9a16a526f787ac99f7f1b6e6aac4cd525d231163
Jan Bordo <janek.bordo@gmail.com>
2018-06-02 18:10:34 +0200

    added bitcoin in pln  live pprices website

---
+++ BitCoin-Prices/js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+
+
+
+    function getPLNData() {
+        var buyPrice = $('#buy-price').html();
+        var sellPrice = $('#sell-price').html();
+
+        $.getJSON('https://blockchain.info/pl/ticker', function (data) {
+            var PLNbuy = data.PLN.buy.toFixed(2);
+            var PLNsell = data.PLN.sell.toFixed(2);
+            var PLNsymbol = data.PLN.symbol;
+            console.log(data.PLN.buy);
+
+            $('#buy-price').text(PLNbuy + PLNsymbol);
+
+            $('#sell-price').text(PLNsell + PLNsymbol);
+
+            if (buyPrice > PLNbuy + PLNsymbol) {
+                $('#buy-arrow').removeClass().addClass('fa fa-arrow-down');
+            } else if (buyPrice < PLNbuy + PLNsymbol) {
+                $('#buy-arrow').removeClass().addClass('fa fa-arrow-up');
+            } else {
+                $('#buy-arrow').removeClass().addClass('fa fa-minus-square');
+            }
+
+            if (sellPrice > PLNsell + PLNsymbol) {
+                $('#sell-arrow').removeClass().addClass('fa fa-arrow-down');
+            } else if (sellPrice < PLNsell + PLNsymbol) {
+                $('#sell-arrow').removeClass().addClass('fa fa-arrow-up');
+            } else {
+                $('#sell-arrow').removeClass().addClass('fa fa-minus-square');
+            }
+
+            console.log('odswiezono');
+
+        })
+    };
+    getPLNData();
+    setInterval(getPLNData, 3000);
+});
