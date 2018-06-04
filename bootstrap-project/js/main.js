c0a19da08b0c108ecaeb469790c618e7e5e9b8a5
Jan Bordo <janek.bordo@gmail.com>
2018-06-04 16:57:56 +0200

    first 3 sections of bootstrap project

--- bootstrap-project/js/main.js
+++ bootstrap-project/js/main.js
@@ -1,3 +1,16 @@
-$('#carouselExampleIndicators').carousel({
-    'interval': 2000,
+$(document).ready(function () {
+    addScrolledClass();
 });
+
+$(window).scroll(function () {
+    addScrolledClass();
+})
+
+function addScrolledClass() {
+    var navHeight = $('#main-nav').outerHeight();
+    if ($(window).scrollTop() >= navHeight) {
+        $('#main-nav').addClass('scrolled');
+    } else {
+        $('#main-nav').removeClass('scrolled');
+    }
+}
